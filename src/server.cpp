7e6b34e3f8dba25970986cb84e1ffbf9a80962f5
Leuxll <yuefungalt@gmail.com>
2024-05-09 21:55:30 -0400

    finished post a file

--- src/server.cpp
+++ src/server.cpp
@@ -82,22 +82,16 @@ int main(int argc, char **argv) {
   std::cout << "Waiting for a client to connect...\n";
 
   while (true) {
-      int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-      if (client_fd < 0) {
-          std::cerr << "Error in accepting client" << std::endl;
-      } else {
-          std::cout << "Client connected\n";
-      }
+    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+    if (client_fd < 0) {
+        std::cerr << "Error in accepting client" << std::endl;
+    } else {
+        std::cout << "Client connected\n";
+    }
 
-      std::thread client_thread(handle_client, client_fd, directory);
-      client_thread.detach();
+    std::thread client_thread(handle_client, std::move(client_fd), directory);
+    client_thread.detach();
   }
-
-  // for (auto& th : threads) {
-  //     if (th.joinable()) {
-  //         th.join();
-  //     }
-  // }
   
   
   close(server_fd);
